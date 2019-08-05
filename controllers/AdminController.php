fc1cb940e14b23d1b5e41c7d4301d52735305d52
Lucas Couto <loc.unb@gmail.com>
2019-08-05 17:48:40 -0300

    Refactoring: optmized code for generating the excel file

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,7 +1,6 @@
 <?php
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class AdminController
@@ -9,49 +8,48 @@ class AdminController
     public static function generateSpreadsheet()
     {
         $sql = new Sql;
-        $clients = $sql->query('SELECT * FROM clients');
+        $clients = $sql->select('SELECT * FROM clients');
 
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
 
         $columns = range('A', 'J');
 
+        /* SET THE COLUMNS WIDTH TO AUTO-SIZE */
         foreach ($columns as $column) {
             $sheet->getColumnDimension($column)->setAutoSize(true);
         }
 
+        /* SET HEADER OF THE TABLE */
+        $header = [
+            'NOME',
+            'DOCUMENTO',
+            'CEP',
+            'ENDEREÇO',
+            'BAIRRO',
+            'CIDADE',
+            'UF',
+            'TELEFONE',
+            'E-MAIL',
+            'ATIVO'
+        ];
 
-        $sheet->setCellValue("A1", 'NOME');
-        $sheet->setCellValue("B1", 'DOCUMENTO');
-        $sheet->setCellValue("C1", 'CEP');
-        $sheet->setCellValue("D1", 'ENDEREÇO');
-        $sheet->setCellValue("E1", 'BAIRRO');
-        $sheet->setCellValue("F1", 'CIDADE');
-        $sheet->setCellValue("G1", 'UF');
-        $sheet->setCellValue("H1", 'TELEFONE');
-        $sheet->setCellValue("I1", 'E-MAIL');
-        $sheet->setCellValue("J1", 'ATIVO');
-
+        $sheet->fromArray($header);
         $sheet->getStyle('A1:J1')->getFont()->setBold(true);
 
-        $index = 2;
+        /* SET THE CONTENT OF THE TABLE, ROW BY ROW */
+        $data = [];
         foreach ($clients as $client) {
-            $sheet->setCellValue("A$index", $client['name']);
-            $sheet->setCellValue("B$index", $client['document']);
-            $sheet->setCellValue("C$index", $client['zip_code']);
-            $sheet->setCellValue("D$index", $client['address']);
-            $sheet->setCellValue("E$index", $client['neighborhood']);
-            $sheet->setCellValue("F$index", $client['city']);
-            $sheet->setCellValue("G$index", $client['state']);
-            $sheet->setCellValue("H$index", $client['phone']);
-            $sheet->setCellValue("I$index", $client['email']);
-            $sheet->setCellValue("J$index", $client['active']);
-
-            $index++;
+            $clientData = [];
+            /* array_slice to skip the 'idclient' field from database */
+            foreach (array_slice($client, 1) as $fieldValue) {
+                array_push($clientData, $fieldValue);
+            }
+            array_push($data, $clientData);
         }
 
+        $sheet->fromArray($data, null, 'A2');
         $writer = IOFactory::createWriter($spreadsheet, 'Xlsx');
-
         $writer->save('php://output');
     }
 }
